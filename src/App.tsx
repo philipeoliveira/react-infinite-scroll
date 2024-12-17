3451ff729c15f3ada010f58111bf6c4e8e9316bf
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-12-17 19:26:07 -0300

    Create structure and styles for home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { Photos } from './pages/Photos';
+import { ExternalLink } from 'lucide-react';
+
 function App() {
-   return <div>Olá, mundo!</div>;
+   return (
+      <div className='flex flex-col gap-10 max-w-6xl mx-auto min-h-screen'>
+         <main className='flex-1 p-6'>
+            <div className='flex flex-col gap-6'>
+               <h1 className='bg-zinc-400 text-zinc-950 text-4xl max-sm:text-3xl font-medium p-2 my-2 text-center rounded-lg'>
+                  Infinite scroll com React
+               </h1>
+               <Photos />
+            </div>
+         </main>
+         <footer>
+            <p className='flex gap-1 justify-center text-sm p-4 border-t border-zinc-400'>
+               Desenvolvido por{' '}
+               <a
+                  href='https://github.com/philipeoliveira'
+                  title='Abrir em nova aba o GitHub do autor Philipe Oliveira'
+                  target='_blank'
+                  className='flex gap-1'
+               >
+                  Philipe Oliveira
+                  <ExternalLink strokeWidth={1.5} size={16} />
+               </a>
+            </p>
+         </footer>
+      </div>
+   );
 }
 
 export default App;
