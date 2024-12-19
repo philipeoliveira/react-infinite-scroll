337f30b027dc8c6f033abf02a36beb87a8dd75e7
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-12-19 10:51:06 -0300

    Decreases padding and h1 in responsive.

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,9 @@ import { ArrowSquareOut } from '@phosphor-icons/react';
 function App() {
    return (
       <div className='flex flex-col gap-10 max-w-6xl mx-auto min-h-screen'>
-         <main className='flex-1 p-6'>
+         <main className='flex-1 p-6 max-md:p-4'>
             <div className='flex flex-col gap-6'>
-               <h1 className='bg-zinc-400 text-zinc-950 text-4xl max-sm:text-3xl font-medium p-2 my-2 text-center rounded-lg'>
+               <h1 className='bg-zinc-400 text-zinc-950 text-4xl max-sm:text-[28px] font-medium p-2 my-2 text-center rounded-lg'>
                   Infinite scroll com React
                </h1>
                <Comments />
