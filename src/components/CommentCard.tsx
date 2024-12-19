402eb44791a32728719bf707c53c25fced297245
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-12-19 10:53:50 -0300

    Refactor card html

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -2,24 +2,22 @@ import { CommentProps } from '../types/comment';
 
 export function CommentCard({ id, name, email, body }: CommentProps) {
    return (
-      <div className='bg-zinc-900 rounded-lg p-6 relative'>
-         <div className='flex flex-col gap-2'>
-            <h3>
-               <span className='font-medium text-zinc-500 capitalize'>Título: </span>
-               {name}
-            </h3>
-            <p>
-               <span className='font-medium text-zinc-500'>E-mail: </span>
-               {email}
-            </p>
-            <p>
-               <span className='font-medium text-zinc-500'>Comentário: </span>
-               {body}
-            </p>
-         </div>
+      <div className='flex flex-col gap-2 bg-zinc-900 rounded-lg py-6 px-8 max-md:px-6 relative'>
          <span className='flex items-center justify-center text-zinc-500 text-sm size-9 bg-zinc-950 absolute right-[calc(50%-18px)] -top-[14px] rounded-full border-4 border-zinc-900'>
             {id}
          </span>
+         <h3>
+            <span className='font-medium text-zinc-500 capitalize'>Título: </span>
+            {name}
+         </h3>
+         <p>
+            <span className='font-medium text-zinc-500'>E-mail: </span>
+            {email}
+         </p>
+         <p>
+            <span className='font-medium text-zinc-500'>Comentário: </span>
+            {body}
+         </p>
       </div>
    );
 }
