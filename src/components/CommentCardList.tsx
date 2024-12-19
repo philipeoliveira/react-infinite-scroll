e61da5de2e95fb24e60f793576e26a595c457630
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-12-19 10:54:47 -0300

    Adds animations for loading

--- src/components/CommentCardList.tsx
+++ src/components/CommentCardList.tsx
@@ -1,14 +1,23 @@
+import { Spinner } from '@phosphor-icons/react';
 import { useCommentsApi } from '../services/useCommentsApi';
 import { CommentCard } from './CommentCard';
 
 export function CommentCardList() {
    const { data, isLoading, isError, error } = useCommentsApi();
 
-   if (isLoading) return <p className='text-center'>Carregando...</p>;
+   if (isLoading) {
+      return (
+         <div className='flex items-center justify-center gap-2'>
+            <Spinner size={22} className='animate-spin-slow' />
+            <p className='text-center my-10 animate-pulse'>Carregando...</p>
+         </div>
+      );
+   }
+
    if (isError) {
       console.log(`Erro: ${error.message}`);
       return (
-         <div className='text-center'>
+         <div className='text-center my-10'>
             <p>Não foi possível se conectar com a API.</p>
             <p>Tente novamente mais tarde.</p>
          </div>
