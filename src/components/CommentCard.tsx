de3e2433d617f90a0252dd7760a162835d555e6d
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-12-20 12:07:06 -0300

    Modifies span to automatically adjust according to id size

--- src/components/CommentCard.tsx
+++ src/components/CommentCard.tsx
@@ -3,7 +3,7 @@ import { CommentProps } from '../types/comment';
 export function CommentCard({ id, name, email, body }: CommentProps) {
    return (
       <div className='flex flex-col gap-2 bg-zinc-900 rounded-lg py-6 px-8 max-md:px-6 relative'>
-         <span className='flex items-center justify-center text-zinc-500 text-sm size-9 bg-zinc-950 absolute right-[calc(50%-18px)] -top-[14px] rounded-full border-4 border-zinc-900'>
+         <span className='flex items-center justify-center text-zinc-500 text-sm p-1 min-w-9 min-h-9 bg-zinc-950 absolute right-[calc(50%-18px)] -top-[14px] rounded-full border-4 border-zinc-900'>
             {id}
          </span>
          <h3>
